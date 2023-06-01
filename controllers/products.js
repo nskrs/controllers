5af6ae6aacc54d3745f5ab62a01eac1686bd90eb
nskrs <nitinkolhe051>
2023-06-01 23:44:30 +0530

    Controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,6 @@
+const Product =require('../models/product')
 
-const products = [];
+
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
       pageTitle: 'Add Product',
@@ -11,12 +12,13 @@ exports.getAddProduct = (req, res, next) => {
   }
 
   exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+   product.save();
     res.redirect('/');
   }
 
   exports.getProduct =  (req, res, next) => {
-    
+    const products = Product.fetchAll();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
